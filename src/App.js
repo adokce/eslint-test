cdcf6b88249bc5ffc6b9ba6a829e599400798ce7
Adnan Karsic <>
2019-12-10 11:53:57 +0100

    test

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function nista() {
-  console.log("nistaaaa");
-}
-
 function App() {
   return (
     <div className="App">
